dd13b485a3597d397c3d61d21c6ff5d07a39f6d0
fgianoli90 <fgianoli90@gmail.com>
2020-04-21 00:01:51 -0400

    update eviction status on dropdown

--- Components/index.js
+++ Components/index.js
@@ -1,19 +1,76 @@
-// Credits to
+// Credits to :
 // Francesca Jeah No Lee https://codepen.io/Francesca_G
 // Raphael Burkhardt https://codepen.io/zibobilo
 
 // Visit https://florida.evictionprotection.org/ to see the interface in action.
 
+// ------------------------------------------------
+
+// Setting the text for each element of the chevrons
+
+// general
+let detailsText = "<h5>- Click for details -</h5>"
+
+// Green chevron elements
+let greenTitle = `<h4>Eviction Active</h4>`
+let greenText = `<div><p>Eviction cases will continue through the county court process, and the sheriff will enforce the removal of tenants (writs of possession) who lose their case.</p></div>`
+
+// Red chevron elements
+let redTitle = `<h4>Eviction Suspended</h4>`
+let redText = `<div><p>- Some part of the eviction court process has been paused or suspended, giving you more time to fight for your right to stay.<br><br>- Removal of tenants who have previously lost their case is paused, either because the clerk has temporarily stopped issuing writs of possession or because the sheriff is not enforcing them</p></div>`
+
+// Blue chevron elements
+let blueTitle = `<h4>Eviction cases active / <br> Tenant removal innactive</h4>`
+let blueText = `<div><p>New and existing eviction cases will continue through the county court process. However, the removal of tenants who lose their case is paused, either because the clerk has temporarily stopped issuing writs of possession or because the sheriff is not enforcing them.</p></div>`
+
+
+// Setting variables to call the Chevrons on demand
+let addChevron = document.querySelector('.statuscolor')
+let greenChevron = `<div class="chevron greencolor" onclick="greenExtra()" id="greench">${greenTitle}${detailsText}</div>`
+let redChevron = `<div class="chevron redcolor" onclick="redExtra()" id="redch">${redTitle}${detailsText}</div>`
+let blueChevron = `<div class="chevron bluecolor" onclick="blueExtra()" id="bluech">${blueTitle}${detailsText}</div>`
+
+addChevron.innerHTML = `${greenChevron}${redChevron}${blueChevron}`
+
+
+// onclick chevrons event handler functions
+function greenExtra() {
+ let x = document.getElementById("greench")
+ if (x.innerHTML == `${greenTitle}${detailsText}`) { 
+   x.innerHTML = `${greenTitle}${greenText}` 
+ } else { 
+   x.innerHTML = `${greenTitle}${detailsText}` 
+ }
+}
+
+function redExtra() {
+  let x = document.getElementById("redch")
+  if (x.innerHTML == `${redTitle}${detailsText}`) {
+    x.innerHTML = `${redTitle}${redText}`
+  } else {
+    x.innerHTML = `${redTitle}${detailsText}`
+  }
+}
+
+function blueExtra() {
+  let x = document.getElementById("bluech")
+  if (x.innerHTML == `${blueTitle}${detailsText}`) { 
+    x.innerHTML = `${blueTitle}${blueText}`
+  } else {
+    x.innerHTML = `${blueTitle}${detailsText}`
+  }
+}
+
 // API Call
-$.getJSON(
-  "https://spreadsheets.google.com/feeds/cells/1xEz5wfWSwXbAgWhgvJxfxSDhNGf5w8hqVpAFbbAViEo/1/public/full?alt=json",
+$.getJSON("https://spreadsheets.google.com/feeds/cells/1xEz5wfWSwXbAgWhgvJxfxSDhNGf5w8hqVpAFbbAViEo/1/public/full?alt=json",
   function (data) {
-    //Success
-    console.log(`# Total Fields : ${data.feed.entry.length}`);
+    // Success
+    // console.log(`# Total Fields : ${data.feed.entry.length}`);
+    
     //Define columns and rows count    
     columns = Number(data.feed.gs$colCount.$t)
     rows = Number(data.feed.gs$rowCount.$t)
-    //console.log(columns)
+    
     //Define database retrieved
     let database = [...data.feed.entry]
 
@@ -32,7 +89,8 @@ $.getJSON(
         allCounty.push(database[i].content.$t)
       }
     }
-
+    
+    //Sorting the counties by alphabetical order
     allCounty.sort()
 
     // Populate dropdown with counties
@@ -41,7 +99,8 @@ $.getJSON(
     for (let i = 0; i < allCounty.length; i++) {
       list.innerHTML += `<option value="${allCounty[i]}">`
     }
-    // console.log(list.innerHTML)
+    
+    //Attributing classes from data cell content to innerHTML
     function stopGoClass(value) {
       if (value == "✖") {
         return "stop"
@@ -49,42 +108,41 @@ $.getJSON(
         return "go"
       }
     }
-    console.log(allCounty)
 
     const selectElement = document.querySelector('.citySelector')
 
-    selectElement.addEventListener('change', (event) => {
+    selectElement.addEventListener('input', (event) => {
+      
       const result = document.querySelector('.timeline')
+      const status = document.querySelector('.statuscolor')
 
       //Only the match between input and database values will retrieve results
       if (allCounty.includes(event.target.value)) {
-        console.log(event.target.value)
+        
+        selectElement.setAttribute("placeholder", event.target.value);
         let row = []
-        let previousCellNum
 
         // building the row for the county selected
         function buildRow(countySelection) {
 
           for (let j = 0; j < database.length; j++) {
-            
 
             if (database[j].content.$t.includes(countySelection) && database[j].gs$cell.col == 1) {
-              console.log(j)
-              console.log(database[j].gs$cell)
-              console.log(database[j].content.$t)
               let sameRow = database[j].gs$cell.row
               //will iterate through the 26 columns for a-z
-              for (let r = j; r < j + columns; r++) {
+              for (let r = j; r < database.length; r++) {
 
                 if (sameRow == database[r].gs$cell.row) {
-                  console.log("hello")
                   row.push({ cellnum: database[r].title.$t.slice(0, 1), text: database[r].content.$t })
+                } else {
+                  break;
                 }
               }
+              break;
             }
           }
         }
-
+        
         buildRow(event.target.value);
         //console.log(row)
         //console.log(colHeads)
@@ -104,20 +162,15 @@ $.getJSON(
         let notes = ""
         let cells = ["R", "U", "V", "W"]
         let counter = 0
+        
         for (n = 2; n < row.length; n++) {
-          
 
           if (row[n].title === "Until") {
             //strng +=`<li class="date">${row[n].title}: ${row[n].text}</li>`
             strng = strng
           } else
-            if (row[n].title=== "Clerk issues summons") {
-            //strng +=`<li class="date">${row[n].title}: ${row[n].text}</li>`
-             strng += `<li class=${stopGoClass(row[n].text)}>${row[n].title} <span class="tooltip" data-tooltip="More information on what this means">?</span></li>`
-          } else
             if (row[n].cellnum < "O" && row[n].cellnum != "H") {
               counter++
-              
               strng += `<li class=${stopGoClass(row[n].text)}>${row[n].title}</li>`
               if (counter === 2) {
                 strng += '<br><li class="textbetween">3 Days </li><br>'
@@ -126,23 +179,23 @@ $.getJSON(
               } else if (counter === 6) {
                 strng += '<br><li class="textbetween"> Cannot Pay </li><br>'
               }
-            } else
-              if (row[n].cellnum === "O") {
+            } else if (row[n].cellnum === "O") {
                 strng += `<li class="date">${row[n].title}: ${row[n].text}</li>`
-              } else
-                if (row[n].title === "Bottom Line") {
-                  bottomLine += `<li class="bottomLine">${row[n].title}: ${row[n].text}</li>`
-                } else
-                  if (cells.includes(row[n].cellnum)) {
-                    links += `<li class="county-links"><a href=${row[n].text}>${row[n].title}</a></li>`
-                  } else
-                    if (row[n].cellnum === "S") {
-                      notes += `<li class="notes">${row[n].title}: ${row[n].text}</li>`
-                    }
+            } else if (row[n].title === "Bottom Line") {
+                if (row[n].text == "Evictions Suspended by Statewide Moratorium") {
+                  bottomLine += `${blueChevron}`}
+                else if (row[n].text == "Evictions Suspended") {
+                  bottomLine += `${redChevron}`}
+                else if (row[n].text == "Evictions Active") {
+                  bottomLine += `${greenChevron}`}
+            } else if (cells.includes(row[n].cellnum)) {
+                links += `<li class="county-links"><a href=${row[n].text}>${row[n].title}</a></li>`
+            } else if (row[n].cellnum === "S") {
+                notes += `<li class="notes">${row[n].title}: ${row[n].text}</li>`
+            }
         }
-        result.innerHTML = `<h2 class="cityTitle">${event.target.value} County</h2>
-      <ol>${bottomLine}<li class="go">Step is Active</li>
-      <li class="stop">Step is Not Active</li><li></li>${strng + links + notes}</ol>`
+        status.innerHTML = `<h2 class="cityTitle">${event.target.value} County</h2>${bottomLine}`
+        result.innerHTML = `<ol>${strng + links + notes}</ol>`
       }
     })
   }
